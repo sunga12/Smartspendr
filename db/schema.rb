e4e9630a3474d3a2e21678956e768aa897cc6bd8
Sunga Thawethe <sungabt@gmail.com>
2023-10-16 15:35:09 +0200

    Rename user_id to author_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_16_102540) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_16_120744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "entities", force: :cascade do |t|
     t.string "name"
     t.float "amount"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_entities_on_user_id"
+    t.index ["author_id"], name: "index_entities_on_author_id"
   end
 
   create_table "entity_groups", force: :cascade do |t|
@@ -43,9 +43,20 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_16_102540) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "entities", "users"
+  add_foreign_key "entities", "users", column: "author_id"
   add_foreign_key "entity_groups", "entities"
   add_foreign_key "entity_groups", "groups"
 end
