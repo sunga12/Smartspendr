6c6ebe23f2b78d79e64dc5604739b96968a91827
Sunga Thawethe <sungabt@gmail.com>
2023-10-16 17:17:30 +0200

    Add Controllers and Views

---
+++ app/controllers/entities_controller.rb
@@ -0,0 +1,20 @@
+class EntitiesController < ApplicationController
+    # only: [:index, :new, :create, :edit, :update, :destroy]
+    def index
+    end
+  
+    def new
+    end
+  
+    def create
+    end
+  
+    def edit
+    end
+  
+    def update
+    end
+  
+    def destroy
+    end
+end
