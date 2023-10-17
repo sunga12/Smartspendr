b586a546d0642f66a6ac45504efd405599c3353d
Sunga Thawethe <sungabt@gmail.com>
2023-10-17 15:28:58 +0200

    Add Author Id to groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_16_120744) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_17_132707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_16_120744) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_groups_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,4 +61,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_16_120744) do
   add_foreign_key "entities", "users", column: "author_id"
   add_foreign_key "entity_groups", "entities"
   add_foreign_key "entity_groups", "groups"
+  add_foreign_key "groups", "users", column: "author_id"
 end
